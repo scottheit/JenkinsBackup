e1beae1d0fd675d52421881dc36a7e3e6f6f323c
root <root@ip-172-31-46-93.ec2.internal>
2015-11-04 16:41:43 +0000

    Add JENKINS_HOME var to script

--- scripts/sdc_jenkins_backup.sh
+++ scripts/sdc_jenkins_backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -xe
 
+cd $JENKINS_HOME
 echo "Hello Jenkins!!"
 git checkout master
-git pull
-git add ../../test.txt
+git pull --rebase
+git add test.txt
 git commit -m "Jenkins backup"
 git push
