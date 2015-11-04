e3f835d618641ac6b542714acc13f2d747dc02e6
root <root@ip-172-31-46-93.ec2.internal>
2015-11-04 15:53:47 +0000

    Change script file path

--- scripts/sdc_jenkins_backup.sh
+++ scripts/sdc_jenkins_backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -xe
 
+cd $(dirname $0)
 echo "Hello Jenkins!!"
 git checkout master
-git add test.txt
-git commit -m "Jenkins backup"
 git pull
+git add ../test.txt
+git commit -m "Jenkins backup"
 git push
